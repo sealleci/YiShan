6ea8d5d81021aeb6be0c47f388bc92f7e7a41ed1
sealleci <1594890315@qq.com>
2022-10-11 21:21:16 +0800

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# 依山 YiShan
+
+## 简介 Introduction
+
+在生态孤岛里负隅顽抗。
+Fight against enemies at the isolated ecological area.
+
+## 作者 Staff
+
+- Sealleci
---
+++ script/compile_less.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+const exec = require("child_process").exec;
+
+(async () => {
+    let raw_dir = "../less/";
+    let raw_out = "../css/";
+    let dir_path = path.join(__dirname, raw_dir);
+    let files = fs.readdirSync(dir_path, { encoding: "utf-8", withFileTypes: true });
+    files.forEach(async (file) => {
+        let file_path = path.join(__dirname, `${raw_dir}${file.name}`);
+        if (fs.statSync(file_path).isFile() && path.extname(file_path).includes("less")) {
+            await exec(`lessc ${dir_path}${file.name} ${path.join(__dirname, raw_out)}${file.name.match(/^([^\.]+)\.[^\.]+$/)[1]}.css --source-map`);
+        }
+    })
+})();
