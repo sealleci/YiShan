478d4ec717647b8b66352b48a4e41b234069f409
sealleci <1594890315@qq.com>
2022-10-19 03:48:43 +0800

    2022-10-19 03:48:42

--- script/git.js
+++ script/git.js
@@ -1,6 +1,8 @@
 const exec = require("child_process").exec;
+const simpleGit = require("simple-git");
 
 (async () => {
+    const git = simpleGit();
     let comment = '';
     if (process.argv.length >= 3) {
         comment = process.argv[2];
@@ -8,7 +10,7 @@ const exec = require("child_process").exec;
         let date = new Date();
         comment = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${(date.getHours() < 10 ? '0' : '') + date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
     }
-    await exec(`git add -A`);
-    await exec(`git commit -m ${comment}`);
+    await git.add('.');
+    await git.commit(comment);
     console.log(`Executed git commands.`);
 })();
