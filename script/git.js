e2a983af28d469f0924d9fe9cc71904ab57bd0c8
sealleci <1594890315@qq.com>
2022-10-19 04:51:04 +0800

    2022-10-19 04:51:03

--- script/git.js
+++ script/git.js
@@ -2,15 +2,15 @@ const exec = require("child_process").exec;
 const simpleGit = require("simple-git");
 
 (async () => {
+    function pad(number) {
+        return (number < 10 ? '0' : '') + number
+    }
     const git = simpleGit();
     let comment = '';
     if (process.argv.length >= 3) {
         comment = process.argv[2];
     } else {
         const date = new Date();
-        function pad(number) {
-            return (number < 10 ? '0' : '') + number
-        }
         comment = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
     }
     await git.add('.');
